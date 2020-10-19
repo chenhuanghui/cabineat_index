33443ccc889068738e6be60e7c771d83224a961b
enZo <inbox.huytran@gmail.com>
2020-10-19 13:19:22 +0700

    add chatfuel connect messenger

--- public/static/fb.js
+++ public/static/fb.js
@@ -8,3 +8,34 @@
   'https://connect.facebook.net/en_US/fbevents.js');
   fbq('init', '962321430930011');
   fbq('track', 'PageView');
+
+  // chat fuel --> connect to messenger of CabinEat Page chat.
+  var div = document.createElement('div');
+  div.className = 'fb-customerchat';
+  div.setAttribute('page_id', '104712234753529');
+  div.setAttribute('ref', '');
+  document.body.appendChild(div);
+  window.fbMessengerPlugins = window.fbMessengerPlugins || {
+    init: function () {
+      FB.init({
+        appId            : '1678638095724206',
+        autoLogAppEvents : true,
+        xfbml            : true,
+        version          : 'v7.0'
+      });
+    }, callable: []
+  };
+  window.fbAsyncInit = window.fbAsyncInit || function () {
+    window.fbMessengerPlugins.callable.forEach(function (item) { item(); });
+    window.fbMessengerPlugins.init();
+  };
+  setTimeout(function () {
+    (function (d, s, id) {
+      var js, fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) { return; }
+      js = d.createElement(s);
+      js.id = id;
+      js.src = "//connect.facebook.net/en_US/sdk/xfbml.customerchat.js";
+      fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
+  }, 0);

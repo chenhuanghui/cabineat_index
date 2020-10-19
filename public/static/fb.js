08092ac35cc8623a8fd048943adbae507515b678
enZo <inbox.huytran@gmail.com>
2020-10-19 12:56:18 +0700

    remove noscript fb

--- public/static/fb.js
+++ public/static/fb.js
@@ -1,12 +1,4 @@
-  (function(d, s, id){
-    var js, fjs = d.getElementsByTagName(s)[0];
-    if (d.getElementById(id)) {return;}
-    js = d.createElement(s); js.id = id;
-    js.src = "https://connect.facebook.net/en_US/sdk.js";
-    fjs.parentNode.insertBefore(js, fjs);
-  }(document, 'script', 'facebook-jssdk'));
-  
-  !function(f,b,e,v,n,t,s)
+!function(f,b,e,v,n,t,s)
   {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
   n.callMethod.apply(n,arguments):n.queue.push(arguments)};
   if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
@@ -16,7 +8,3 @@
   'https://connect.facebook.net/en_US/fbevents.js');
   fbq('init', '962321430930011');
   fbq('track', 'PageView');
-
-<noscript><img height="1" width="1" style="display:none"
-  src="https://www.facebook.com/tr?id=962321430930011&ev=PageView&noscript=1"
-/></noscript>

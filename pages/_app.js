3300c7da0563f24a0f453ec3f2cdc3ee6e770995
enZo <inbox.huytran@gmail.com>
2021-02-06 10:44:16 +0700

    update layout

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,4 @@
+import '../public/assets/css/cabin-style.css'
 import 'react-multi-carousel/lib/styles.css';
 import "react-notion/src/styles.css";
 import "prismjs/themes/prism-tomorrow.css"; // only needed for code highlighting

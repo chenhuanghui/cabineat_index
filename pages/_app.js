3ee80a5a7b7e25126c0e2f4ad218b9168ccc784b
enZo <inbox.huytran@gmail.com>
2021-02-12 23:17:04 +0700

    update react-notion style

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
 import '../public/assets/css/cabin-style.css'
 import 'react-multi-carousel/lib/styles.css';
 import "react-notion/src/styles.css";
-import "prismjs/themes/prism-tomorrow.css"; // only needed for code highlighting
+
+// core styles shared by all of react-notion-x (required)
+import 'react-notion-x/src/styles.css'
+
+// used for code syntax highlighting (optional)
+import 'prismjs/themes/prism-tomorrow.css'
+
+// used for collection views (optional)
+import 'rc-dropdown/assets/index.css'
+
+// used for rendering equations (optional)
+import 'katex/dist/katex.min.css'
 
 export default function App({ Component, pageProps }) {
     return <Component {...pageProps} />

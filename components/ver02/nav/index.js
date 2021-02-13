894dd09a34b22cca34a3982858c735b6fd756f6e
enZo <inbox.huytran@gmail.com>
2021-02-13 12:22:23 +0700

    ..

--- components/ver02/nav/index.js
+++ components/ver02/nav/index.js
@@ -1,105 +1,49 @@
 import React from 'react';
-import Router from 'next/router';
 import Link from 'next/link'
-import Modal from 'react-modal';
-import MenuModal from '../../menu-modal'
+import Annoucement from '../../announcement';
 
-export default class Nav extends React.Component {
-    constructor(props){
-        super(props);
-        this.state = {
-            openMenu: false
-        }
-    }
+export default function NavVer02() {
+    return (
+        <div className="nav grid grid-template-columns-1fr bg-white">
+            <Annoucement
+                url="https://www.facebook.com/cabineat.vn"
+                title="Chương trình hỗ trợ đồng hành cùng doanh nghiệp Việt, giảm tác động covid-19"
+            />
 
-    componentDidMount() {    
-    }
-    
-    toggleMenu = () => {
-        if ($("#navbar").hasClass("show")) {
-            $("#navbar").removeClass("show")
-        } else {
-            $("#navbar").addClass("show")
-        }        
-    }
-
-    render() {           
-        return(
-            <>
-            <nav className="navbar navbar-expand-lg navbar-light" id="topnav">
-                <div className="container">          
-                    <a className="navbar-brand mr-auto mt-2" href="/">
-                        <img src="../assets/img/logo.svg" alt="..." className="navbar-brand-img" style={{maxHeight: "2rem"}}/>
-                    </a>
-
-                    {/* <div className="navbar-user d-flex d-md-none" id="navbar" >
-                        <span className="fe fe-user-check " style={{fontSize: "25px"}}></span>
-                    </div> */}
-
-                    <div className="navbar-collapse collapse ml-auto order-last" id="navbar">
-                        <ul className="navbar-nav mr-lg-auto">
-                            <li className="nav-item">
-                                <Link href="/events">
-                                    <a className="nav-link">Sự kiện</a>
-                                </Link>
-                            </li>  
-                            {/* <li className="nav-item">
-                                <a className="nav-link disable" href="#">Kiến thức</a>
-                            </li> */}
-                        </ul>
-                        <hr className="d-md-none"/>
-                        <ul className="navbar-nav mr-lg-auto">
-                            <li className="nav-item">
-                                <a href="http://my.cabineat.vn" className="nav-link">Truy cập</a>
-                            </li>  
-                        </ul>
-                    </div>
-                    
-                    <button className="navbar-toggler ml-auto" onClick={this.toggleMenu}>
-                        <span className="navbar-toggler-icon"></span>
-                    </button>      
-                    
+            <div className="nav-wrapper grid container-cabin padding-y-24 navbar-light border-bottom align-items-center">
+                <div className="nav-item-1 logo">
+                    <img src="../assets/img/logo.svg" alt="..." className="navbar-brand-img" style={{maxHeight: "2rem"}}/>
                 </div>
-            </nav>            
-            
-            {this.state.openMenu 
-                ? <MenuModal 
-                    onClosed = {()=> {
-                        this.setState({openMenu: false})
-                    }}
-                />
-                : null
+
+                <div className="nav-item-2 nav-toggle justify-self-end align-self-end">
+                    <span className="navbar-toggler-icon"></span>
+                </div>
+
+                <div className="nav-item-3 nav-list-wrapper">
+                    asdfsad
+                </div>
+            </div>
+
+            <style>{`
+            .nav-wrapper {
+                grid-template-columns: 1fr 1fr;
             }
-        <style jsx>{`
-        .nav-link {
-            color: #1a1a1a !important;
-            font-weight: bold !important;
-        }
+            .nav-item-2 {
 
-        .nav-link :hover {
-            cursor: pointer;
-            text-decoration: underline;
-        }
+            }
+            .nav-item-3 {
+                display:none;
+            }
+            @media (min-width:768px){
+                .nav-wrapper {grid-template-columns: 150px 1fr}
+                .nav-item-2 {display: none;}
+                .nav-item-3 {
+                    display: inline-grid; 
+                    justify-self: end;
+                }
+            }
 
-        .nav-link.disable {
-            color: #6e84a3 !important;
-            text-decoration: none;
-        }
-
-        .nav-link.disable :hover {
-            cursor: default
-        }
-
-        .navbar{
-            position: sticky;
-            width: 100%;
-            top: 0px;
-            z-index: 99999
-        }
-        
-        `}</style>
-            </>
-        )
-    }
-
+            `}</style>
+        </div>
+    )
 }
--- pages/index_new.js
+++ pages/index_new.js
@@ -1,4 +1,7 @@
 import Head from 'next/head'
+import FooterNew from '../components/footer-new'
+import Nav from '../components/nav-new'
+import NavVer02 from '../components/ver02/nav'
 
 export default function Index() {
     return (
@@ -8,11 +11,20 @@ export default function Index() {
                 <title>CabinEat - Trang chủ</title>
                 <meta name="description" content="CabinEat - Trang chủ" />
                 <meta name="keywords" content="cabineat,nha hang so, online restaurant, delivery" />
+                <meta property="og:title" content="CabinEat"></meta>
+                <meta property="og:type" content="website"></meta>
+                <meta property="og:locale" content="vi_VN"></meta>
+                <meta property="og:url" content="https://cabineat.vn"></meta>
+                {/* <meta property="og:image" content={`${post.cover ? post.cover[0].url : ""}`}></meta> */}
+                <meta property="og:description" content="Giải pháp delivery trực tiếp dành cho nhà hàng"></meta>
+
                 <script async src="https://cdn.splitbee.io/sb.js"></script>
             </Head>
+            <NavVer02 />
             <div className="container-cabin">
-
+                
             </div>
+            <FooterNew />
         </div>
     )
 }

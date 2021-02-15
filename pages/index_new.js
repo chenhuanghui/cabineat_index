306e9fb479c48a6fc306e2ec9f75d7e01900b59d
enZo <inbox.huytran@gmail.com>
2021-02-15 13:13:18 +0700

    ..

--- pages/index_new.js
+++ pages/index_new.js
@@ -199,6 +199,49 @@ export default function Index() {
                     </div>
                 </div>
 
+                <div className="delivery bg-soft padding-y-24">
+                    <div className="container-cabin delivery-wrapper grid grid-gap-24-16">
+                        <div className="delivery-item-1 grid grid-gap-24-16">
+                            <div className="delivery-title">
+                                <p className="caption-extra font-weight-bold letter-spacing-n1px">Local delivery</p>
+                                <p className="title">Offer a delivery experience to match the aggregators. Bring your own drivers or connect to our network</p>
+                            </div>
+                            <div className="delivery-content-wrapper grid grid-gap-24-16">
+                                <div className="grid grid-gap-8-8">
+                                    <p className="font-weight-bold">Update customers in real-time</p>
+                                    <p className="">Keep customers in the loop with automatic notifications at every step of the journey.</p>
+                                </div>
+                                <div className="grid grid-gap-8-8">
+                                    <p className="font-weight-bold">Live tracking screen</p>
+                                    <p className="">Reduce phone calls and set expectations with a live map displaying the driver's current location and expected arrival time.</p>
+                                </div>
+                                <div className="grid grid-gap-8-8">
+                                    <p className="font-weight-bold">Delivery zones</p>
+                                    <p className="">Cover your variable delivery costs with dynamic delivery fees calculated by postcode lookup.</p>
+                                </div>
+                                <div className="grid grid-gap-8-8">
+                                    <p className="font-weight-bold">Connect to 15+ couriers</p>
+                                    <p className="">Auto-dispatch orders to the courier of your choice through our delivery integration partner, Orkestro.</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="delivery-item-2 justify-self-center align-self-center">
+                            <video width= "100%" autoPlay loop muted playsInline>
+                                <source src="https://ucarecdn.com/4f13583f-1dcb-4d82-9a35-cd7ff0a5c9e3/delivery3.mp4" type="video/mp4"/>
+                            </video>
+                        </div>
+                    </div>                    
+                </div>
+
+                <div className="container-cabin margin-top">
+                    <div className="grid border rounded">
+                        <img src="https://ucarecdn.com/d822eaf0-1a91-4786-85f2-429902a55fd9/41842833_1374809102654983_861106054828654592_o.jpg" width="100%" className="rounded-top"/>
+                        <div className="padding-24 text-justify">
+                            <p> Urban Chocolatier is fast growing dessert shop franchise with 6 stores in the UK. With ambitious growth plans, they chose StoreKit to power their direct delivery and click & collect ordering channels.</p>
+                        </div>
+                    </div>
+                </div>
+
             </div>
             <FooterNew />
 
@@ -208,6 +251,11 @@ export default function Index() {
                     -169deg
                     , rgb(248, 246, 243) 14%, rgb(255, 255, 255) 59%);
             }
+
+            .bg-soft {
+                background: rgb(248, 246, 243);
+            }
+
             .main-container {
                 padding-top: 24px;
             }
@@ -237,6 +285,11 @@ export default function Index() {
                 padding-top: 100px;
             }
 
+            .delivery {
+                padding-top: 48px !important;
+                padding-bottom: 48px !important;
+            }
+
             @media (min-width: 768px) {
                 .hero .hero-description {
                     width: 70%;
@@ -264,6 +317,10 @@ export default function Index() {
                 .features {
                     padding-top: 100px;
                     grid-template-columns: 1fr 1fr;
+                }
+
+                .delivery-wrapper {
+                    grid-template-columns: 2fr 3fr;
                 }
             }
 

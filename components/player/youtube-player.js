d43e102fa9ae6ed78aa6eb14e823e2f99cf84acb
enZo <inbox.huytran@gmail.com>
2021-02-17 17:33:35 +0700

    ..

--- components/player/youtube-player.js
+++ components/player/youtube-player.js
@@ -1,6 +1,7 @@
 import { useEffect } from 'react';
 import YouTube from '@u-wave/react-youtube';
-import ReactPlayer from 'react-player'
+// import ReactPlayer from 'react-player'
+import ReactPlayer from 'react-player/lazy'
 
 export default function YoutubePlayer({videoID}) {
     useEffect(()=>{
@@ -9,7 +10,7 @@ export default function YoutubePlayer({videoID}) {
     },[])
     return (         
         <div className="player-wrapper">
-            <YouTube
+            {/* <YouTube
                 video={videoID}
                 autoplay
                 muted
@@ -21,7 +22,22 @@ export default function YoutubePlayer({videoID}) {
                 showRelatedVideos={false}
                 playsInline={true}
                 showInfo={false}                    
+            /> */}
+            <ReactPlayer 
+                className='player'
+                url={`https://www.youtube.com/watch?v=${videoID}`}
+                width="100%"
+                heigh="100%"
+                controls={true}
+                mostbranding={`true`}
+                loop={true}                
+                muted={false}
+                playsinline={true}
+
             />
+            <style jsx>{`
+
+            `}</style>
         </div>
     )
 }

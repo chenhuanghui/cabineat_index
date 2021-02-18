9152db74ab9a54031653707d05de28989b8be77f
enZo <inbox.huytran@gmail.com>
2021-02-18 23:25:22 +0700

    fix nav

--- components/ver02/nav/index.js
+++ components/ver02/nav/index.js
@@ -4,13 +4,22 @@ import Annoucement from '../../announcement';
 
 export default function NavVer02() {
     const toggleMenu = ()=> {        
-        if(!$('.nav-list-wrapper').hasClass('show')) 
-            $('.nav-list-wrapper').addClass('show')
-        else $('.nav-list-wrapper').removeClass('show')        
+        if(!$('.nav-wrapper').hasClass('show')) {
+            $('.nav-wrapper').addClass('show')
+            $('.nav-list-wrapper').css('display','grid')
+            $('.sub-menu').css('display','grid')
+            $('.btn-create-store').css('display','grid')
+        }
+        else {
+            $('.nav-wrapper').removeClass('show')
+            $('.nav-list-wrapper').css('display','none')
+            $('.sub-menu').css('display','none')
+            $('.btn-create-store').css('display','none')
+        }
     }
     return (
         <div className="nav grid grid-template-columns-1fr bg-white">
-            <div className="nav-top nav-top-wrapper container-cabin grid align-items-center padding-x">
+            {/* <div className="nav-top nav-top-wrapper container-cabin grid align-items-center padding-x">
                 <div className="nav-top-item-1 justify-self-start">
                     <a href="#" className="font-weight-bold small mr-2 text-dark">
                         <span className="fe fe-user mr-2"></span>Hỗ trợ</a>
@@ -23,25 +32,27 @@ export default function NavVer02() {
                         <span className="fe fe-chevron-down"></span>
                     </a>
                 </div>
-            </div>
+            </div> */}
             <div className="navbar-light border-bottom">
-                <div className="nav-wrapper grid container-cabin align-items-center">
-                    <div className="nav-item-1 logo">
-                        <img src="../assets/img/logo.svg" alt="..." className="navbar-brand-img" style={{maxHeight: "2rem"}}/>
+                <div className="nav-wrapper grid container-cabin align-items-center grid-gap-16-16">
+                    <div className="nav-item-1 logo grid">
+                        <img src="../assets/img/logo.svg" alt="..." className="navbar-brand-img" style={{maxHeight: "2rem"}}/>                        
+                        <span className="navbar-toggler-icon ml-auto" onClick={toggleMenu}></span>                    
                     </div>
 
-                    <div className="nav-item-2 nav-toggle justify-self-end align-self-end">
-                        <span className="navbar-toggler-icon" onClick={toggleMenu}></span>
+                    <div className="nav-list-wrapper grid grid-gap-12-12">                        
+                        <a className="nav-item-link font-weight-bold text-dark" href="#">nhahang.so</a>
+                        <a className="nav-item-link font-weight-bold text-dark" href="#">cabinpos</a>
+                        <a className="nav-item-link font-weight-bold text-dark" href="#">delivery</a>
+                        <a className="nav-item-link font-weight-bold text-dark" href="#">thanh toán</a>                        
                     </div>
 
-                    <div className="nav-item-3 nav-list-wrapper">                        
-                        <a className="nav-item-link align-self-center font-weight-bold text-dark" href="#setup">Thiết lập</a>
-                        <a className="nav-item-link align-self-center font-weight-bold text-dark" href="#intro">Giới thiệu</a>
-                        <a className="nav-item-link align-self-center font-weight-bold text-dark" href="#hub-order">Cổng đặt hàng</a>
-                        <a className="nav-item-link align-self-center font-weight-bold text-dark" href="#cabin-pos">Xử lý đơn</a>
-                        <a className="nav-item-link align-self-center font-weight-bold text-dark" href="#delivery">Vận chuyển</a>
-                        <a className="nav-item-link align-self-center font-weight-bold text-dark" href="#payment">Thanh toán</a>
-                        <a className="nav-item-link align-self-center btn btn-create-store text-white" style={{padding: "6px 12px !important"}}>Tạo tài khoản miễn phí</a>
+                    <a className="nav-item-link btn-primary btn btn-create-store text-white" style={{padding: "6px 12px !important"}}>Tạo tài khoản miễn phí</a>
+
+                    <div className="grid padding-y grid-gap-12-12 sub-menu">
+                        <a className="font-weight-bold text-dark" href="/events">Sự kiện</a>
+                        <a className="font-weight-bold text-dark" href="/about">Giới thiệu</a>
+                        <a className="font-weight-bold text-dark" href="https://my.cabineat.vn">Đăng nhập</a>
                     </div>
                 </div>
             </div>
@@ -50,58 +61,62 @@ export default function NavVer02() {
             <style>{`
             .nav {
                 box-shadow: 0px 8px 20px rgba(0, 0, 0, 0.06);
-            }
-            .nav-top-wrapper {
-                padding: 12px 12px !important;     
-                display: none;
-            }
-
-            .nav-wrapper {
-                grid-template-columns: 1fr 25px;
-                padding: 24px 12px !important;                                
-            }
-            .nav-item-2 {
-
             }            
 
-            .nav-item-3 {
-                padding: 12px 0px 12px 0px;
+            .nav-wrapper {
+                grid-template-columns: 1fr;
+                padding: 24px 12px !important;
+            }            
+
+            .logo {
+                grid-template-columns: 1fr 25px;
+                border-bottom: 1px;
+            }
+
+            .nav-list-wrapper, .sub-menu, .btn-create-store {
                 display: none;
             }
             
             .nav-item-3.show {
                 display: inline-grid !important;
                 transition: opacity 1s ease-out;
-            }
+            }                        
 
-            .nav-item-3 a{
-                display: block;
-                padding: 6px 0px;
-            }
-
-            .btn-create-store {
-                background-color: #1F4DF5;                
-            }
-
-            @media (min-width:992px){
-                .nav-top {
-                    display: grid;
-                    grid-template-columns: 1fr 1fr;
+            @media (min-width:768px){
+                .logo {
+                    grid-template-columns: 1fr;
+                }
+                .navbar-toggler-icon {
+                    display: none;
                 }
                 .nav-wrapper {
-                    grid-template-columns: 150px 1fr;
+                    grid-template-columns: 150px 1fr auto;
                     padding: 12px 12px !important;
                 }
-                .nav-item-2 {display: none;}
-                .nav-item-3 {
-                    display: flex !important;
-                    justify-self: end !important;                    
-                }
-                .nav-item-3 a{
-                    padding: 0px 6px !important;
+                .nav-list-wrapper {
+                    grid-template-columns: repeat(auto-fit, minmax(min-content, 100px));
                 }
                 .nav-item-link {
-                    margin-right: 10px;
+                    text-align: center;
+                }
+                
+                .nav-list-wrapper, .sub-menu, .btn-create-store {
+                    display: grid;
+                }
+
+                .sub-menu {
+                    grid-column: 1/4;
+                    grid-row: 1/2;
+                    display: flex;
+                    margin-left: auto;
+                }
+                .sub-menu a{
+                    border-right: 1px solid #e3ebf6;
+                    padding-right: 12px;
+                    font-size: 12px !important;
+                }
+                .sub-menu a:last-child {
+                    border-right: none !important;
                 }
             }
 

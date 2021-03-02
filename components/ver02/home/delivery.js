3eaecea58c9bc03977e6ec593d44e6e6ca119ec8
enZo <inbox.huytran@gmail.com>
2021-03-02 18:30:11 +0700

    update data delivery blockg

--- components/ver02/home/delivery.js
+++ components/ver02/home/delivery.js
@@ -5,6 +5,8 @@ const NOTION_DELIVERY_ID = 'e9273c8a7565489eb4e630bde8e8658d'
 export default function Delivery() {
     const [data, setData] = useState(null)
     const [videoSelectedID, setVideoSelectedID] = useState()
+    const [imageSelected, setImageSelected] = useState()
+
     useEffect(()=>{
         async function fetchData() {
             const contents = await fetch(
@@ -17,15 +19,17 @@ export default function Delivery() {
                 }                
             });
             setData(temp)        
-            setVideoSelectedID(temp[0].youtubeID)
+            // setVideoSelectedID(temp[0].youtubeID)
+            setImageSelected(temp[0].thumb && temp[0].thumb[0].url)
         }
         fetchData()
     },[])
 
-    const handleSelectChoice = (itemID, videoID) => {
+    const handleSelectChoice = (itemID, thumbURL) => {
         $('.delivery-item-choice.selected').removeClass('selected')
         $(`.delivery-item-choice.${itemID}`).addClass('selected')
-        setVideoSelectedID(videoID)
+        // setVideoSelectedID(videoID)
+        setImageSelected(thumbURL)
         var video = document.querySelector("#delivery-video");
         video.scrollIntoView({ behavior: 'smooth', block: 'end'})
     }
@@ -40,17 +44,18 @@ export default function Delivery() {
                     </div>
                     <div className="delivery-content-wrapper grid">
                         {data && data.map((item,index)=>(
-                            <div className={`grid grid-gap-8-8 padding rounded delivery-item-choice ${item.id} ${index===0 ? "selected" : null}`} key={item.id} onClick={()=>{handleSelectChoice(item.id, item.youtubeID)}}>
+                            <div className={`grid grid-gap-8-8 padding rounded delivery-item-choice ${item.id} ${index===0 ? "selected" : null}`} key={item.id} onClick={()=>{handleSelectChoice(item.id, item.thumb && item.thumb[0].url)}}>
                                 <p className="font-weight-bold">{item.title}</p>
-                                <p className="">{item.sub}</p>
+                                <p className="text-justify">{item.sub}</p>
                             </div>
                         ))}                    
                     </div>
                 </div>        
-                <div className="delivery-item-2 justify-self-center align-self-center">
+                <div className="delivery-item-2 justify-self-center align-self-center">                    
                     <div className="justify-self-center align-self-center">
                         <div className="delivery-video video" id="delivery-video">
-                            <YoutubePlayer videoID={videoSelectedID} />     
+                            {/* <img className="rounded" url={imageSelected} width="100%" /> */}
+                            <img className="rounded" src={imageSelected} width="100%"/>
                         </div>               
                     </div>
                 </div>
@@ -60,13 +65,16 @@ export default function Delivery() {
                 padding-top: 48px !important;
                 padding-bottom: 48px !important;
             }
+            .delivery-item-choice:hover {
+                cursor: pointer;
+            }
             .delivery-item-choice.selected {
                 background-color: #E4EBF6;
             }
 
             @media (min-width: 768px) {
                 .delivery-wrapper {
-                    grid-template-columns: 2fr 3fr;
+                    grid-template-columns: 1fr 1fr;
                 }
             }
             `}</style>              
